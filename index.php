632a454063aa436bbac94b63c4d212afc33eed88
Alexis van der Merwe <demius@gmail.com>
2014-12-26 21:17:00 +0200

    Added Bootstrap and RequireJS

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 require 'vendor/autoload.php';
-require 'inc/service-locator.php';
 
 require 'entities/User.php';
 require 'entities/Role.php';
@@ -10,26 +9,58 @@ require 'entities/mappers/UserMapper.php';
 
 $app = new \Slim\Slim();
 
-$app->get('/users', function () {
-    $spot = get_spot_instance();
-    $mapper = $spot->mapper('Entities\User');
-    echo json_encode($mapper->all()->toArray());
+$app->container->singleton('db_context', function(){
+    $spot_cfg = new Spot\Config();
+    $spot_cfg->addConnection('mysql', [
+        'dbname' => 'eyewitness-web',
+        'user' => 'root',
+        'password' => '',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql',
+    ]);
+    return new \Spot\Locator($spot_cfg);
 });
 
-$app->get('/users/:id', function ($id) {
+$app->get('/users', function () use($app) {
+    $mapper = $app->db_context->mapper('Entities\User');
+    respond(json_encode($mapper->all()->toArray()));
+});
+
+$app->post('/users', function() use($app) {
+    $mapper = $app->db_context->mapper('Entities\User');
+
+    $entity = $mapper->build([
+        'display_name' => $app->request->params('display_name'),
+        'email_address' => $app->request->params('email_address'),
+        'password' => $app->request->params('password')
+    ]);
+
+    if($mapper->save($entity)){
+        return respond(json_encode($entity));
+    }
+    respond('Failed to save the new user.', 500);
+});
+
+$app->get('/users/:id', function ($id) use($app) {
+    $mapper = $app->db_context->mapper('Entities\User');
+    respond(json_encode($mapper->get($id)));
+});
+
+$app->put('/users/:id', function($id) use($app) {
+
+});
+
+$app->delete('/users/:id', function($id) use($app) {
     $spot = get_spot_instance();
     $mapper = $spot->mapper('Entities\User');
     echo json_encode($mapper->get($id));
 });
 
-$app->post('/users', function(){
-    /*
-    $entity = $mapper->build([
-        'display_name' => 'Alexis van der Merwe',
-        'email_address' => 'demius@gmail.com',
-        'password' => 'Willy21@'
-    ]);*/
-    //$result = $mapper->save($entity);
-});
-
 $app->run();
+
+function respond($body, $http_status = 200, $content_type = 'application/json') {
+    $app = new \Slim\Slim();
+    $app->response->status($http_status);
+    $app->response->headers->set('Content-Type', $content_type);
+    echo $body;
+}
---
+++ js/main.js
@@ -0,0 +1,30 @@
+require.config({
+    baseUrl: 'js/vendor',
+    paths: {
+        'app': '../app',
+        'templates': '../../templates',
+        'text': 'text',
+        'jquery': 'jquery-1.11.2.min',
+        'bootstrap': 'bootstrap.min',
+        'underscore': 'underscore-min',
+        'backbone': 'backbone-min',
+        'marionette': 'backbone.marionette.min'
+    },
+    shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        },
+        'marionette': {
+            deps: ['backbone'],
+            exports: 'Marionette'
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+    //urlArgs: "bust=" + (new Date()).getTime()
+});
